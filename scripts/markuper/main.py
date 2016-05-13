a08217871123742682c7a99a981d02d0801854f5
Himura Kazuto <glagol15@gmail.com>
2016-05-13 23:04:21 +0300

    beta

--- scripts/markuper/main.py
+++ scripts/markuper/main.py
@@ -1,16 +1,26 @@
 import os
+import re
 
 dialogs_path = "..\\..\\dialogs"
 pictures_path = "..\\..\\pictures"
 texts_path = "..\\..\\texts"
 
+env_re_find = re.compile(r"((-.*\n)+)(\n*.*)\n*((-.*\n)+)")
+env_re_replace = \
+    r"\\begin{dialog}\n\1\\end{dialog}\n\n\\begin{monolog}\3\n\\end{monolog}\n\n\\begin{dialog}\n\4\\end{dialog}"
+
+rep_re_find = re.compile(r'(^-)(.*?\n)((^-)(.*?\n))?', re.MULTILINE)
+rep_re_replace = r"\\X\2\\R\5"
+
 
 def process_dialog():
     if title in existing_dialogs:
         return
-    print(number, title, txt_path, pic_path)
-
-
+    # number, title, txt_path, pic_path
+    text = open(txt_path, encoding="utf-8").read() + "\n"
+    text = re.sub(env_re_find, env_re_replace, text)
+    text = re.sub(rep_re_find, rep_re_replace, text)
+    print(text + "\n------------\n")
 
 existing_pics = {file.split(".")[0] for file in os.listdir(pictures_path)}
 existing_texts = {file.split(".")[0] for file in os.listdir(texts_path)}
